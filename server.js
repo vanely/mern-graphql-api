83685403f947614606126f66e80ecedc35c25eba
vanely <vanelys94@gmail.com>
2019-09-16 19:25:44 -0400

    graphql back-end api initialization

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const GraphQLServer = require('graphql-yoga');
+const {GraphQLServer} = require('graphql-yoga');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const uri = process.env.ATLAS_URI;
 const connection = mongoose.connection;
@@ -7,9 +8,13 @@ mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
 // ----------------------------------------------------
 // GraphQL setup from docs
+
+const Todo = require('./model/todo');
+
 const typeDefs = `
     type Query {
         hello(name: String): String!
+        todos: [Todo]
     }
     type Todo {
         id: ID!
@@ -18,13 +23,31 @@ const typeDefs = `
     }
     type Mutation {
         createTodo(text: String!): Todo
+        updateTodo(id: ID!, complete: Boolean!): Boolean
+        removeTodo(id: ID!): Boolean
     }
 `;
 
 const resolvers = {
     Query: {
         hello: (_, { name }) => `Hello ${name || 'world'}`,
+        todos: () => Todo.find()
     },
+    Mutation: {
+        createTodo: async (_, { text }) => {
+            const todo = new Todo({ text, complete: false });
+            await todo.save();
+            return todo;
+        },
+        updateTodo: async (_, { id, complete }) => {
+            await Todo.findByIdAndUpdate(id, { complete });
+            return true;
+        },
+        removeTodo: async (_, { id }) => {
+            await Todo.findByIdAndRemove(id);
+            return true;
+        }
+    }
 }
 
 const server = new GraphQLServer({typeDefs, resolvers});
